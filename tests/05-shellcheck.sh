c1a67c6596b3185caa44b5225ab8cadb75591692
Paolo <paolo.gigante.@rackspace.co.uk>
2018-03-13 12:33:10 +0000

    Fixed the git diff commands

--- tests/05-shellcheck.sh
+++ tests/05-shellcheck.sh
@@ -5,13 +5,11 @@ g='\033[32m'
 
 EXITCODE=0
 AUTERDIR="$(cd "$(dirname "$0")" ; cd ../ ; pwd -P )"
+CHANGEDFILES="$(find / -name CHANGEDFILES)"
 
 # Create a list of script files to be checked. These are files 
-echo "DEBUG: Current directory: $(pwd)"
-echo "DEBUG: AUTERDIR: ${AUTERDIR}"
-echo "DEBUG: CHANGEDFILES location: $(find / -name CHANGEDFILES)"
-if [[ -f "${AUTERDIR}"/CHANGEDFILES ]]; then
-  ALLFILES=$(find "${AUTERDIR}" -type f -not -path '*/\.*' | egrep "$(cat CHANGEDFILES | xargs | tr ' ' '|')")
+if [[ -f "${CHANGEDFILES}" ]]; then
+  ALLFILES=$(find "${AUTERDIR}" -type f -not -path '*/\.*' | egrep "$(cat ${CHANGEDFILES} | xargs | tr ' ' '|')")
   
 else
   ALLFILES=$(find "${AUTERDIR}" -type f -not -path '*/\.*')
@@ -24,8 +22,8 @@ done
 echo "${SCRIPTSTOTEST}"
 
 # Add non-shebang scripts manually
-grep -q "auter.aptModule" "${AUTERDIR}"/CHANGEDFILES && SCRIPTSTOTEST+=" auter.aptModule "
-grep -q "auter.yumdnfModule" "${AUTERDIR}"/CHANGEDFILES && SCRIPTSTOTEST+=" auter.yumdnfModule "
+grep -q "auter.aptModule" "${CHANGEDFILES}" && SCRIPTSTOTEST+=" auter.aptModule "
+grep -q "auter.yumdnfModule" "${CHANGEDFILES}" && SCRIPTSTOTEST+=" auter.yumdnfModule "
 
 # Custom shellcheck exclusions
 SHELLCHECK_EXCLUSIONS=",SC2102,SC2124,SC2155,SC2148"
